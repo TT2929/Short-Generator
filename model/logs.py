1cdac9d027e6fe6cac49df0943b5b2f28c4751a2
TT2929 <be_2960@i.softbank.jp>
2025-08-04 00:53:35 +0900

    Update logs.py

--- model/logs.py
+++ model/logs.py
@@ -1,10 +1,5 @@
 from pathlib import Path
 import os
-os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
-os.environ["MKL_NUM_THREADS"] = "1"
-os.environ["VECLIB_NUM_THREADS"] = "1"
-os.environ["NUMEXPR_NUM_THREADS"] = "1"
 import seaborn as sns
 from io import StringIO
 import pandas as pd
